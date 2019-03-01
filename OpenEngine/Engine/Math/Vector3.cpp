4eb0965949a2545d5025febfb2b477160a77bee5
okarpov <okarpov@saber3d.ru>
2019-03-01 12:42:00 +0300

    .

--- OpenEngine/Engine/Math/Vector3.cpp
+++ OpenEngine/Engine/Math/Vector3.cpp
@@ -5,11 +5,11 @@ Vector3::Vector3()
 	x = y = z = 0;
 }
 
-Vector3::Vector3(const Vector3& other)
-{
-	x = other.x; y = other.y; z = other.z;
-}
-
+//Vector3::Vector3(const Vector3& other)
+//{
+//	x = other.x; y = other.y; z = other.z;
+//}
+//
 Vector3::Vector3(float x)
 {
 	this->x = y = z = x;
@@ -22,37 +22,31 @@ Vector3::Vector3(float x, float y, float z)
 	this->z = z;
 }
 
-//
+
 //Vector3& Vector3::operator=(const Vector3& other)
 //= default;
 
-Vector3& Vector3::operator=(const Vector3& other)
-{
-	x = other.x;
-	y = other.y;
-	z = other.z;
-
-	return *this;
-}
+//Vector3& Vector3::operator=(const Vector3& other)
+//= default;
 
 Vector3 Vector3::operator+(const Vector3& other) const
 {
-	return Vector3(x + other.x, y + other.y, z + other.z);
+	return {x + other.x, y + other.y, z + other.z};
 }
 
 Vector3 Vector3::operator-(const Vector3& other) const
 {
-	return Vector3(x - other.x, y - other.y, z - other.z);
+	return {x - other.x, y - other.y, z - other.z};
 }
 
 Vector3 Vector3::operator*(const Vector3& other) const
 {
-	return Vector3(x * other.x, y * other.y, z * other.z);
+	return {x * other.x, y * other.y, z * other.z};
 }
 
 Vector3 Vector3::operator*(float x) const
 {
-	return Vector3(this->x * x, y * x, z * x);
+	return {this->x * x, y * x, z * x};
 }
 
 Vector3& Vector3::operator+=(const Vector3& other)
--- OpenEngine/Engine/Math/Vector3.h
+++ OpenEngine/Engine/Math/Vector3.h
@@ -6,11 +6,11 @@ class Vector3
 public:
 	
 	Vector3();
-	Vector3(const Vector3& other);
+	//Vector3(const Vector3& other);
 	Vector3(float x);
 	Vector3(float x, float y, float z);
 
-	Vector3& operator=(const Vector3& other);
+	//Vector3& operator=(const Vector3& other);
 	Vector3 operator+(const Vector3& other) const;
 	Vector3 operator-(const Vector3& other) const;
 	Vector3 operator*(const Vector3& other) const;
